2aef8c71e575df55e4ef88d00dff5ad7b1cc674a
Warren Kenny <wkenny@riotgames.com>
2016-04-20 11:15:44 +0100

    Mirrored some type exports in gh module for convenience

--- src/endpoint/gh_commit.erl
+++ src/endpoint/gh_commit.erl
@@ -2,12 +2,12 @@
 -author( "Warren Kenny <warren.kenny@gmail.com>" ).
 
 %% Query Functions
--export( [list/2, by_signature/3] ).
+-export( [list/2, by_sha/3] ).
 
 %% Type Exports
 -type commit() 		:: map().
--type signature()	:: string().
--export_type( [commit/0, signature/0] ).
+-type sha()			:: string().
+-export_type( [commit/0, sha/0] ).
 
 %%
 %%	List all commits made on the specified repository
@@ -19,6 +19,6 @@ list( Repository, State ) ->
 %%
 %%	Get details on a single commit identified by its SHA1 signature
 %%
--spec by_signature( gh_repo:repository(), signature(), gh:state() ) -> { ok, commit() } | { error, term() }.
-by_signature( Repository, Signature, State ) ->
-	gh_request:get( ["repos", gh_repo:owner( Repository ), gh_repo:name( Repository ), "commits", Signature ], State ).
+-spec by_sha( gh_repo:repository(), sha(), gh:state() ) -> { ok, commit() } | { error, term() }.
+by_sha( Repository, Sha, State ) ->
+	gh_request:get( ["repos", gh_repo:owner( Repository ), gh_repo:name( Repository ), "commits", Sha ], State ).
--- src/gh.erl
+++ src/gh.erl
@@ -4,9 +4,13 @@
 -export( [init/0, init/1, init/2, request_module/2] ).
 -include_lib( "gh.hrl" ).
 
--type auth() 	:: atom() | { atom(), string() } | { atom(), string(), string() }.
--type state() 	:: #gh_state{}.
--export_type( [state/0, auth/0] ).
+-type auth() 		:: atom() | { atom(), string() } | { atom(), string(), string() }.
+-type state() 		:: #gh_state{}.
+-type repository() 	:: gh_repo:repository().
+-type branch() 		:: gh_branch:branch().
+-type commit() 		:: gh_commit:commit().
+-type commit_sha() 	:: gh_commit:sha().
+-export_type( [state/0, auth/0, repository/0, branch/0, commit/0, commit_sha/0] ).
 
 %%
 %%  Initialize gh without an authentication token. Errors will occur if an attempt is made
